4b7cbb4491d2efb1bc824b9ebe9d8d97352aaeb5
Wicho2000 <lgutierrez15212@plataforma-utslp.net>
2025-01-31 18:15:15 -0800

    Fusionando cambios con la última versión remota

--- suma.sh
+++ suma.sh
@@ -3,6 +3,7 @@
 echo -n "Ingresa el primer numero: "
 read num1
 
+<<<<<<< HEAD
 echo -n "Ingresa el segundo numero: "
 read num2
 
@@ -11,3 +12,10 @@ resta=$((num1 - num2))
 
 echo "La suma de $num1 y $num2 es: $suma"
 echo "La resta de $num1 menos $num2 es : $resta"
+=======
+suma=$((num1 + num2))
+resta=$(($suma - num3))
+echo "La suma de $num1 y $num2 es $suma"
+echo "La resta de $suma y $num3 es $resta"
+
+>>>>>>> cf55386 (Guardando cambios antes de hacer pull)
