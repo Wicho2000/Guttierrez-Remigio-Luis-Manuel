f5ed67c42ab3a16c6ba2502ac930862fccaa8483
Gutierrez Remigio Luis Manuel <lgutierrez15212@plataforma-utslp.net>
2025-03-22 13:55:32 -0600

    pagina

---
+++ Parcial3/Producto-03.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "18a3c1a80f2d45b2be214e04ec926296"
+
+@app.route('/')
+def obtener_juegos():
+    url = f"https://api.rawg.io/api/games?key={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    juegos_filtrados = [
+        {"nombre": juego["name"], "lanzamiento": juego["released"]}
+        for juego in data.get("results", [])
+    ]
+    
+    # Renderiza la página HTML con los juegos
+    return render_template("index.html", juegos=juegos_filtrados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
